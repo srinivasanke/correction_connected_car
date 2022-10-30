c7194c6c38ea402611045cbf03f094c63e842c56
srinivasanke <30660932+srinivasanke@users.noreply.github.com>
2022-10-30 16:58:30 +0100

    Update dealer-car-list.component.ts

--- src/app/components/home/dealer-car-list/dealer-car-list.component.ts
+++ src/app/components/home/dealer-car-list/dealer-car-list.component.ts
@@ -5,45 +5,30 @@ import {MatTableDataSource} from '@angular/material/table';
 import { Router } from '@angular/router';
 import { CarListService } from '../services/car-list.service';
 import { DatePipe } from '@angular/common';
-import { FormControl, FormGroup } from '@angular/forms';
 import { NativeDateAdapter } from '@angular/material/core';
+import {carListConstants} from '../dealer-car-list/car-list'
+import moment from 'moment'
+import { FormGroup, FormControl,ReactiveFormsModule, Validators } from '@angular/forms';
 
 
-export interface carList {
-  carId:number;
-  modelName: string;
-  vin: string;
-  registrationNumber: string;
-  dongleId:string;
-  soldOn: Date;
-  
-}
-const carData: carList[] =  [
-{carId:1, modelName:"Audi", vin:"uhjyu12h3hg45nbm4",registrationNumber:"AP03 CB 2341",dongleId:"087",soldOn:"12-08-2022"},
-  {carId:2, modelName:"Audi", vin:"uhjyu12h3hg45nbm6",registrationNumber:"AP03 CB 2351",dongleId:"086",soldOn:"13-08-2022"},
-  {carId:3, modelName:"Tata", vin:"uhjyu12h3hg45nbm5",registrationNumber:"AP03 CB 2361",dongleId:"085",soldOn:"14-08-2022"},
-  {carId:4, modelName:"Audi", vin:"uhjyu12h3hg45nbm9",registrationNumber:"AP03 CB 2371",dongleId:"084",soldOn:"15-08-2022"},
-  {carId:5, modelName:"BMW", vin:"uhjyu12h3hg45nbm3",registrationNumber:"AP03 CB 2391",dongleId:"083",soldOn:"16-08-2022"},
-  {carId:6, modelName:"Audo", vin:"uhjyu12h3hg45nbm7",registrationNumber:"AP03 CB 2301",dongleId:"082",soldOn:"17-08-2022"},
-  {carId:7, modelName:"Audo", vin:"uhjyu12h3hg45nbm0",registrationNumber:"AP03 CB 2331",dongleId:"081",soldOn:"18-08-2022"}
-];
+
+
 
 @Component({
   selector: 'app-dealer-car-list',
   templateUrl: './dealer-car-list.component.html',
   styleUrls: ['./dealer-car-list.component.css']
 })
+
 export class DealerCarListComponent implements OnInit {
   displayedColumns: string[] = ['carId','modelName', 'vin', 'registrationNumber', 'dongleId','soldOn'];
-  dataSource = new MatTableDataSource<carList>(carData);
+  
   pipe: DatePipe | undefined;
-  // filterData:Date | undefined;
-  // filterDates:boolean = false;
-  filterForm = new FormGroup({
-    fromDate: new FormControl(),
-    toDate: new FormControl(),
-});
-  filterDate = new MatTableDataSource<carList> (carData);
+  filterData: any = [];
+  travelersData: any = [];
+  finalData: any = [];
+  search: any = { query: ''};
+  filterForm!: FormGroup;
 
 get fromDateVal() { 
   return this.filterForm.get('fromDate')?.value;
@@ -56,80 +41,66 @@ get toDateVal() {
   @ViewChild(MatPaginator) paginator!:MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
   constructor(private route:Router,private allCar: CarListService) {
-//     this.allCar.getCar().subscribe(res => {
-//       this.dataSource = new MatTableDataSource(res.data.cars);
-      // this.filterDate = new MatTableDataSource(res.data.cars.soldOn);
-      this.dataSource.paginator = this.paginator;
-      this.dataSource.sort = this.sort;
-      // console.log(res);
-      // console.log(this.dataSource);
-//   })
-  // this.pipe = new DatePipe('en');
-  // this.dataSource.filterPredicate = (data) =>{
-  //   if (this.fromDate && this.toDate) {
-  //     return data.soldOn >= this.fromDate && data.soldOn <= this.toDate;
-  //   }
-  //   return true;
-  // }
     }
     
   ngOnInit(): void {
-    // this.data(this.filterDates)
-    this.pipe = new DatePipe('en');
-    this.dataSource.filterPredicate = (data, filter) =>{
-      if (this.fromDateVal && this.toDateVal) {
-        return data.soldOn >= this.fromDateVal && data.soldOn <= this.toDateVal;
+    this.filterForm = new FormGroup(
+      {
+        fromDate: new FormControl('', [Validators.required]),
+        toDate: new FormControl('', [Validators.required]),
+        searchData : new FormControl('', [Validators.required]),
       }
-      return true;
+    );
+    // this.data(this.filterDates)
+    const data: any = carListConstants.carData;
+    this.travelersData = [...data];
+    this.filterData = [...data];
+    this.finalData = [...this.filterData];
+    console.log(this.finalData);
     }
-  
+  reverseAndTimeStamp(dateString: string) {
+    const reverse = new Date(dateString.split("-").reverse().join("-"));
+    return reverse.getTime();
+    }
+
+  searchCars(): void { 
+    this.filterData = this.travelersData.filter((item: any) => {
+       if (JSON.stringify(this.filterForm.value.searchData && JSON.stringify(this.toDateVal) && JSON.stringify(this.fromDateVal))) {
+        return (this.filterByDate(item) && this.filterByName(item));
+      } 
+    });
+    this.filterData = this.filterData
+
   }
 
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-   
+  
+  filterByDate(item: any): any {
+    
+     let fromDate = moment(this.fromDateVal).format('DD-MM-YYYY');
+     let toDate = moment(this.toDateVal).format('DD-MM-YYYY');
+      return this.reverseAndTimeStamp(item.soldOn) >= this.reverseAndTimeStamp(fromDate) && this.reverseAndTimeStamp(item.soldOn) <= this.reverseAndTimeStamp(toDate)
+
+
   }
+
+  filterByName(item: any): any {
+    return item.modelName.toLowerCase() === this.filterForm.value.searchData.toLowerCase();
+    
+  }
+
   openCarRegister(){
     this.route.navigate(['/home/car-register']);
     
   }
-  applyDateFilter() {
-    // this.dataSource.filter = "2005-05-12";
-    // console.log(this.fromDate._i)
-    // console.log(this.filterForm.value);
-    this.dataSource.filter = JSON.stringify(this.fromDateVal, this.toDateVal);
-    this.dataSource.data =this.dataSource.data.filter(e=> e.soldOn >= this.filterForm.value.fromDate && e.soldOn <= this.filterForm.value.toDate);
-   console.log(this.dataSource.filter )
-  }
+ 
 
-  
-  // applyDateFilter() {
-    // this.dataSource.data = carData;
-    // console.log("car",this.dataSource.filteredData)
-    // const fromDate = this.filterForm.get('fromDate')?.value;
-    // const toDate = this.filterForm.get('toDate')?.value;
-    // this.dataSource.filteredData = this.dataSource.filteredData.filter(e=>e.soldOn > fromDate.value && e.soldOn < toDate.value ) ;
-    // this.filterForm.value.fromDate
-    // this.filterDates = true
-    // this.dataSource.filterPredicate = (data) =>{
-    //   if (this.filterDates = true) {
-    //     return data.soldOn >= this.fromDate && data.soldOn <= this.toDate;
-    //   }
-    //   return true;
-    // }
-    // console.log(this.filterForm.value)
-  // }
-// data(dateFilter:any){
-//   this.allCar.getCar().subscribe(res => {
-//     this.dataSource = new MatTableDataSource(res.data.cars);
-//     // this.filterDate = new MatTableDataSource(res.data.cars.soldOn);
-//     this.dataSource.paginator = this.paginator;
-//     this.dataSource.sort = this.sort;
-//     // console.log(res);
-//     console.log(this.dataSource.filteredData[0].soldOn);
-// })
-// }
 }
-
-
+export interface carList {
+  carId:number;
+  modelName: string;
+  vin: string;
+  registrationNumber: string;
+  dongleId:string;
+  soldOn: any;
+  
+}
